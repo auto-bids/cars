b0f90b7e5162231685cf3187a823043dd18d116a
Barti <bartosz.majkowski@wp.pl>
2024-01-17 18:28:37 +0100

    database connection added

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	router := gin.Default()
+
+	//routes.SessionRoute(router)
+
+	errRouter := router.Run(os.Getenv("PROFILES_URI"))
+	if errRouter != nil {
+		panic(errRouter)
+	}
+}
