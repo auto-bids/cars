2c294c558176897cd0ed6f7542969e23df5e75c9
Barti <bartosz.majkowski@wp.pl>
2024-01-24 19:22:38 +0100

    get one car

--- routes/car_routes.go
+++ routes/car_routes.go
@@ -8,6 +8,7 @@ import (
 func CarsRoute(router *gin.Engine) {
 	profiles := router.Group("/cars")
 	{
+		profiles.GET("/offer/:id", controllers.GetOneOffer)
 		profiles.POST("/page/:page", controllers.GetOffers)
 		profiles.POST("/add/:email", controllers.PostOffer)
 		profiles.DELETE("/delete/:email", controllers.DeleteOffer)
